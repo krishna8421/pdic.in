044b3e140426a0fd30cdcee12a04a6e7ce7d3605
Sunitadexcel <sunita@dexceldigitalhub.com>
2024-12-09 16:18:00 +0530

    update title and description home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Benefits from "@/components/Benefits";
+import type { Metadata } from "next";
 import Booking from "@/components/Booking";
 import Hero from "@/components/Hero";
 import HowTo from "@/components/HowTo";
 import Services from "@/components/Services";
 import About from "@/components/About";
+
+export const metadata: Metadata = {
+  title: "Dental Clinic in Wakad and Pimpri Chinchwad | Prateek Dental Care & Implant Centre",
+  description: "Prateek Dental Care & Implant Centre offers expert dental services in Wakad and Pimpri Chinchwad. From dental implants to braces and root canals, our experienced team provides top-quality care. Call 7229832893 to book an appointment today!",
+};
 
 const Home = () => (
   <>
